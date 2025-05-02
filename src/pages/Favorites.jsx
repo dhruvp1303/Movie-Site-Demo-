c4b70a6f47d6772ec70a159eed7baded2cdf5cc6
dhruvp1303 <dhruvp1303@gmail.com>
2025-05-02 02:27:36 -0400

    Delete src directory

--- src/pages/Favorites.jsx
+++
@@ -1,9 +0,0 @@
-import "../css/Favorites.css"
-function Favorites() {  
-    return (    
-        <div className="favorites-empty">
-            <h1>No favorites yet</h1>
-        </div>
-    )
-}
-export default Favorites
