1dd9f840cf3d5adc7b638d63674867966f0d8518
dhruvp1303 <dhruvp1303@gmail.com>
2025-05-02 02:26:50 -0400

    Add files via upload

---
+++ src/services/api.js
@@ -0,0 +1,13 @@
+const API_KEY = "9f01e042271a425646d5045c9eed5a31"
+const BASE_URL = "https://api.themoviedb.org/3"
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`)
+    const data = await response.json()
+    return data.results
+}
+export const getMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`)
+    const data = await response.json()
+    return data.results
+}
